e59d149ada7acc589e930a1c445425ed85a9706e
Monijo <mokajozefowicz@gmail.com>
2022-07-10 19:42:48 +0200

    parametrize tests with pytest.

--- config/tests/test_example1.py
+++ config/tests/test_example1.py
@@ -10,6 +10,23 @@ def test_example():
 def test_example2():
     assert 1 == 2
 
+
 @pytest.mark.slow
 def test_example3():
     assert 1 == 1
+
+
+TEST_DIV_DATA = (
+    (8, 2, 4),
+    (0, 9, 0),
+    (7, 2, 3.5)
+)
+
+
+def div(a, b):
+    return a / b
+
+
+@pytest.mark.parametrize("a,b, result", TEST_DIV_DATA)
+def test_div(a, b, result):
+    assert div(a, b) == result
